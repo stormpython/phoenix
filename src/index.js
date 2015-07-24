115102b5fe09f6336a8e7125200156f991465938
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-07-24 13:10:25 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 define(function (require) {
-  return require("main");
+  return require("phx");
 });
--- src/require.config.js
+++ src/require.config.js
@@ -2,6 +2,7 @@ require.config({
   baseUrl: "src/components",
   paths: {
     jubilee: "../../bower_components/jubilee/build/jubilee.min",
+    phx: "main"
   },
   shim: {},
   packages: []
