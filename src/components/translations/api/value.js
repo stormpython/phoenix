b1c77547e1700c406f11c86d6ff241b155cb58e5
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-08-13 16:01:33 -0700

    adding an error handler to the value.js function

--- src/components/translations/api/value.js
+++ src/components/translations/api/value.js
@@ -1,5 +1,9 @@
 define(function () {
   return function (defaultVal) {
+    if (typeof defaultVal !== "string") {
+      throw new Error("default value must be of type string");
+    }
+
     return function (_) {
       if (!arguments.length) return function (d) { return d[defaultVal]; };
       if (typeof _ !== "string") throw new Error("A string is expected");
