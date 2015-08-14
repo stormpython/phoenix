70267c19205a7ed009faced7d100ddb19c1e68cb
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-08-13 17:29:45 -0700

    in progress changes for the translation api. this breaks some code functionality because its not complete

--- src/components/translations/bar.js
+++ src/components/translations/bar.js
@@ -1,6 +1,50 @@
 define(function (require) {
-  return {
-    margin: { top: 20, right: 20, bottom: 20, left: 20 },
+  require("jubilee"); // Includes D3
+  var margin = require("src/components/translations/defaults/margin");
+  var scale = require("src/components/translations/defaults/scale");
+  var xAxis = require("src/components/translations/defaults/x_axis");
+  var yAxis = require("src/components/translations/defaults/y_axis");
+  var stack = require("src/components/translations/defaults/stack");
+  var clipPath = require("src/components/translations/defaults/clippath");
+  var zeroLine = require("src/components/translations/defaults/zeroline");
+  var values = function (d) { return d; };
+  var rects = {
+    groupClass: "rects",
+    cssClass: "bars",
+    //x: function (d, i, j, scale) {
+    //  return scale(xValue.call(null, d, i));
+    //},
+    //y: function (d, i, j, scale) {
+    //  return scale(yValue.call(null, d, i));
+    //},
+    //width: function (d, i, j, scale, data) {
+    //  return scale.range()[1] / data.length;
+    //},
+    //height: function (d, i, j, scale) {
+    //  return scale.range()[0] - scale(yValue.call(null, d, i));
+    //},
+    rx: 0,
+    ry: 0,
+    //fill: function (d, i) { return color(i); },
+    //stroke: function (d, i) { return color(i); },
+    strokeWidth: 0,
+    opacity: 1
+  };
 
-  }
+  return {
+    margin: require("src/components/translations/api/object")(margin),
+    x: require("src/components/translations/api/value")("x"),
+    y: require("src/components/translations/api/value")("y"),
+    values: require("src/components/translations/api/function")(values),
+    stacked: require("src/components/translations/api/boolean")(true),
+    color: require("src/components/translations/api/function")(d3.scale.category10()),
+    xScale: require("src/components/translations/api/object")(scale),
+    yScale: require("src/components/translations/api/object")(scale),
+    xAxis: require("src/components/translations/api/object")(xAxis),
+    yAxis: require("src/components/translations/api/object")(yAxis),
+    stack: require("src/components/translations/api/object")(stack),
+    clipPath: require("src/components/translations/api/object")(clipPath),
+    zeroLine: require("src/components/translations/api/object")(zeroLine),
+    rects: require("src/components/translations/api/object")(rects)
+  };
 });
--- src/components/translations/line.js
+++ src/components/translations/line.js
@@ -1,3 +1,45 @@
 define(function (require) {
+  require("jubilee"); // Includes D3
+  var margin = require("src/components/translations/defaults/margin");
+  var defined = require("src/components/translations/defaults/defined");
+  var scale = require("src/components/translations/defaults/scale");
+  var xAxis = require("src/components/translations/defaults/x_axis");
+  var yAxis = require("src/components/translations/defaults/y_axis");
+  var clipPath = require("src/components/translations/defaults/clippath");
+  var zeroLine = require("src/components/translations/defaults/zeroline");
+  var lines = {
+    groupClass: "paths",
+    lineClass: "line",
+    //stroke: function (d, i) { return color(i); },
+    strokeWidth: 3,
+    opacity: 1,
+    interpolate: "linear",
+    tension:  0.7
+  };
+  var circles = {
+    show: true,
+    groupClass: "circle layer",
+    circleClass: "circle",
+    //fill: function (d, i, j) { return color(j); },
+    stroke: null,
+    radius: 5,
+    strokeWidth: 3
+  };
 
+  return {
+    margin: require("src/components/translations/api/object")(margin),
+    x: require("src/components/translations/api/value")("x"),
+    y: require("src/components/translations/api/value")("y"),
+    defined: require("src/components/translations/api/function")(defined),
+    //interpolate: require("src/components/translations/api/string")("linear"),
+    color: require("src/components/translations/api/function")(d3.scale.category10()),
+    xScale: require("src/components/translations/api/object")(scale),
+    yScale: require("src/components/translations/api/object")(scale),
+    xAxis: require("src/components/translations/api/object")(xAxis),
+    yAxis: require("src/components/translations/api/object")(yAxis),
+    clipPath: require("src/components/translations/api/object")(clipPath),
+    zeroLine: require("src/components/translations/api/object")(zeroLine),
+    lines: require("src/components/translations/api/object")(lines),
+    circles: require("src/components/translations/api/object")(circles)
+  };
 });
