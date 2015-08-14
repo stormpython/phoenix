050a5afe2cb837aa60a22a87362ed1a9cb1be13a
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-08-14 09:19:49 -0700

    adding interpolate option to line charts

--- src/components/translations/line.js
+++ src/components/translations/line.js
@@ -31,7 +31,7 @@ define(function (require) {
     x: require("src/components/translations/api/value")("x"),
     y: require("src/components/translations/api/value")("y"),
     defined: require("src/components/translations/api/function")(defined),
-    //interpolate: require("src/components/translations/api/string")("linear"),
+    interpolate: require("src/components/translations/api/string")("linear"),
     color: require("src/components/translations/api/function")(d3.scale.category10()),
     xScale: require("src/components/translations/api/object")(scale),
     yScale: require("src/components/translations/api/object")(scale),
