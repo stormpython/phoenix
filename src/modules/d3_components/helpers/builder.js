b0b0e90bd30f3b0f738192a5fe6b33664944cda7
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-11-05 13:07:48 -0800

    renaming attrs to obj for clarity

--- src/modules/d3_components/helpers/builder.js
+++ src/modules/d3_components/helpers/builder.js
@@ -1,8 +1,8 @@
 define(function (require) {
   var d3 = require('d3');
 
-  return function builder(attrs, func) {
-    if (typeof attrs !== 'object' || Array.isArray(attrs)) {
+  return function builder(obj, func) {
+    if (typeof obj !== 'object' || Array.isArray(obj)) {
       throw new Error('builder expects a javascript Object ({}) as its first argument');
     }
 
@@ -10,7 +10,7 @@ define(function (require) {
       throw new Error('builder expects a function as its second argument');
     }
 
-    d3.entries(attrs).forEach(function (d) {
+    d3.entries(obj).forEach(function (d) {
       if (typeof func[d.key] === 'function') {
         func[d.key](d.value);
       }
