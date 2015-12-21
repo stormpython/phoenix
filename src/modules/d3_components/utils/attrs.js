0e70614acb1e9f245f7df8515889b1404a4b4653
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-12-20 16:05:46 -0800

    using builder in attrs function

--- src/modules/d3_components/utils/attrs.js
+++ src/modules/d3_components/utils/attrs.js
@@ -1,17 +1,11 @@
 define(function (require) {
-  var d3 = require('d3');
+  var builder = require('src/modules/d3_components/utils/builder');
 
   return function attrs(func, generator) {
     return function (attr, value) {
       if (!value && attr instanceof String) { return func[attr](); }
 
-      if (!value && attr instanceof Object) {
-        d3.entries(attr).forEach(function (d) {
-          if (typeof func[d.key] === 'function') {
-            func[d.key](d.value);
-          }
-        });
-      }
+      if (!value && attr instanceof Object) { builder(attr, func); }
 
       if (value && attr instanceof String) { func[attr](value); }
 
